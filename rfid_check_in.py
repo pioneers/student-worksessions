b4df1e226c584b150966689b00100dfa9fabd00c
j-m-h <james.m.hulett@sbcglobal.net>
2015-11-11 20:15:51 -0800

    Added ability to look up id numbers

--- rfid_check_in.py
+++ rfid_check_in.py
@@ -6,11 +6,11 @@ import sys
 import time
 import datetime
 
-"""import httplib2
+import httplib2
 import pprint
 
 from apiclient.discovery import build
-from decrypt import leaky_decrypt"""
+from decrypt import id_decrypt
 
 #TODO: convert from number to name (and put the name in the worksheet instead)
 def enter_info(wks, id_num, time):
@@ -39,47 +39,61 @@ if __name__ == "__main__":
 	scope = ['https://spreadsheets.google.com/feeds']
 	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
 
-	"""http = httplib2.Http()
+	print('Fetching user information...')
+	sys.stdout.flush()
+	http = httplib2.Http()
 	http = credentials.authorize(http)
-
-	#return build('admin', 'directory_v1', http=http)
-
 
 	directory_service = build('admin', 'directory_v1', http=http)
 
 	all_users = []
 	page_token = None
 	params = {'customer': 'my_customer'}
+	ids = {}
 
 	while True:
-	  try:
-	    if page_token:
-	      params['pageToken'] = page_token
-	    current_page = directory_service.users().list(**params).execute()
+		try:
+			if page_token:
+				params['pageToken'] = page_token
+			current_page = directory_service.users().list(**params).execute()
 
-	    all_users.extend(current_page['users'])
-	    page_token = current_page.get('nextPageToken')
-	    if not page_token:
-	      break
-	  except errors.HttpError as error:
-	    print 'An error occurred: %s' % error
-	    break
+			all_users.extend(current_page['users'])
+			page_token = current_page.get('nextPageToken')
+			if not page_token:
+				break
+	  	#except errors.HttpError as error:
+		except Exception as error:
+			print 'An error occurred: %s' % error
+			sys.stdout.flush()
+			break
 	for user in all_users:
+		#print user
+		name = user['name']
+		if 'organizations' in user:
+			try:
+				org = user['organizations'][0]
+				txt = org['costCenter']
+				txt = str(txt)
+				#out = name['givenName'] + ','+ name['familyName'] + ',' + leaky_decrypt(txt) + '\n'
+				name = name['givenName'] + ' ' + name['familyName']
+				ids.update({leaky_decrypt(txt): name})
+			except:
+				print("error loading " + str(name['givenName']))
+				sys.stdout.flush()
 
-	  #print user
-	  name = user['name']
-	  
-	  if 'organizations' in user:
-	    try:
-	      org = user['organizations'][0]
-	      txt = org['costCenter']
-	      txt = str(txt)
-	      out = name['givenName'] + ','+ name['familyName'] + ',' + leaky_decrypt(txt) + '\n'
-	      print out
-	      fil.write(out)
-	    except:
-	      print("error with " + str(name['givenName']))"""
+	while True:
+		print 'Search for an id number:'
+		sys.stdout.flush()
+		search_num = raw_input()
+		if search_num in ids.keys():
+			print ids[search_num]
+		else:
+			print "Didn't find that id..."
+		sys.stdout.flush()
+		raw_input() # Wait until they hit enter to continue
 
+
+	exit() # Just want to test the user information part right now
 	# Open the worksheet
 	gc = gspread.authorize(credentials)
 	#TODO: put proper worksheet here

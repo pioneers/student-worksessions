5fa44a65bdb91d0f2411cf46e4e9de9242fc53d2
Jiana Huang <jhuang99@berkeley.edu>
2016-01-11 01:20:25 -0800

    migration integer

--- db/migrate/20160104041413_change_columns_user_id_worksession.rb
+++ db/migrate/20160104041413_change_columns_user_id_worksession.rb
@@ -1,6 +1,6 @@
 class ChangeColumnsUserIdWorksession < ActiveRecord::Migration
   def change
-  	change_column :worksessions, :created_by, :integer
+  	change_column :worksessions, :created_by, 'integer USING CAST(created_by AS integer)''
   	remove_column :worksessions, :team
 
   end

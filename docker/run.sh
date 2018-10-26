b2cd7a32ca4f1b4c32871e8ea7e79675134ba55a
scottnuma <scott.numamoto@gmail.com>
2018-10-25 21:13:45 -0700

    Fix run.sh documentation

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,24 @@
 echo "Note:This script should be run from student_worksessions/, not student_worksessions/docker/\n";
 
+docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev /bin/bash 
+
 # Run a Docker image
-docker run\
-        -it\ # To open a shell we use interactive mode with tty
-        --rm\ # Upon quitting from our shell, clean up and remove the container
-        -p 3000:3000\ # Connect port 3000 inside the container to port 3000 on our machine
-        -v "$PWD:/usr/src/app"\ # Map /usr/src/app in the container to the files in our present working directory
-        numascott/pie-student-worksessions:dev\ # Run this Docker image
-        /bin/bash\ # Execute this command on the image to open a shell
+# docker run
 
+# To open a shell we use interactive mode with tty
+# -it 
 
+# Upon quitting from our shell, clean up and remove the container
+# --rm
+
+# Connect port 3000 inside the container to port 3000 on our machine
+# -p 3000:3000 
+
+# Map /usr/src/app in the container to the files in our present working directory
+# -v "$PWD:/usr/src/app" 
+
+# Run this Docker image
+# numascott/pie-student-worksessions:dev 
+
+# Execute this command on the image to open a shell
+# /bin/bash

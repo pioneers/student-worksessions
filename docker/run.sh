f3d2b5fdf282220a4c8e34da79f56f93f851d220
jehanyang <jehanyang@gmail.com>
2018-10-27 14:50:07 -0700

    windows error, needed to change PWD to explicit file address in docker/run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,7 @@
+
 echo "Note:This script should be run from student_worksessions/, not student_worksessions/docker/\n";
 
-docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev /bin/bash 
+winpty docker run -it --rm -p 3000:3000 -v "C:/Users/Jehan/Documents/PiE/Infrastructure/student_worksessions/usr/src/app" numascott/pie-student-worksessions:dev /bin/bash 
 
 # Run a Docker image
 # docker run
@@ -15,7 +16,7 @@ docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-wo
 # -p 3000:3000 
 
 # Map /usr/src/app in the container to the files in our present working directory
-# -v "$PWD:/usr/src/app" 
+# -v "$C:\Users\Jehan\Documents\PiE\Infrastructure\student_worksessions\usr\src\app" 
 
 # Run this Docker image
 # numascott/pie-student-worksessions:dev 

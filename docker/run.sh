7d22464908fa6bd6a1d1bb1fe004d544b875126e
scottnuma <scott.numamoto@gmail.com>
2018-10-29 21:25:10 -0700

    Modify Docker run script for Windows

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,12 @@
 
 echo "Note:This script should be run from student_worksessions/, not student_worksessions/docker/\n";
 
-winpty docker run -it --rm -p 3000:3000 -v "C:/Users/Jehan/Documents/PiE/Infrastructure/student_worksessions/usr/src/app" numascott/pie-student-worksessions:dev /bin/bash 
+if [[ "$OSTYPE" == "msys"* ]]; then
+	echo "Running for Git Bash on Windows"
+   winpty docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev bash
+else
+   docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev bash
+fi
 
 # Run a Docker image
 # docker run

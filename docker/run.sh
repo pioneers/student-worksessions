ea04a59e1593db752f7b96d439ba978a4a936d28
scottnuma <scott.numamoto@gmail.com>
2018-10-31 00:06:30 -0700

    DRY docker run command

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,12 @@
 
 echo "Note:This script should be run from student_worksessions/, not student_worksessions/docker/\n";
 
+docker_command="docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev bash"
 if [[ "$OSTYPE" == "msys"* ]]; then
 	echo "Running for Git Bash on Windows"
-   winpty docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev bash
+   eval winpty $docker_command
 else
-   docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev bash
+   eval $docker_command
 fi
 
 # Run a Docker image

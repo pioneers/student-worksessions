ee94ed4ee1170bdf27981abd32db642bf363231f
scottnuma <scott.numamoto@gmail.com>
2018-11-07 20:52:21 -0800

    Update rails and dependencies

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 
 echo "Note:This script should be run from student_worksessions/, not student_worksessions/docker/\n";
 
-docker_command="docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:dev bash"
+docker_command="docker run -it --rm -p 3000:3000 -v "$PWD:/usr/src/app" numascott/pie-student-worksessions:sec bash"
 if [[ "$OSTYPE" == "msys"* ]]; then
 	echo "Running for Git Bash on Windows"
    eval winpty $docker_command
